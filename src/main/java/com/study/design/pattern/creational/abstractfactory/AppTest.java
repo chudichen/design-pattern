3e885403b970a64f01492819b17a0fa4335181d5
chudichen <chudichen.foxmail.com>
2019-04-10 15:52:33 +0800

    添加工厂方法注释

--- src/main/java/com/study/design/pattern/creational/abstractfactory/AppTest.java
+++ src/main/java/com/study/design/pattern/creational/abstractfactory/AppTest.java
@@ -3,7 +3,8 @@ package com.study.design.pattern.creational.abstractfactory;
 import org.junit.jupiter.api.Test;
 
 /**
- * 不需要关心（无需导入）具体的现实，只关注产品族
+ * 感觉像是工厂方法的升级版，将产品分类为产品族，
+ * 一个工厂可以生产自己产品族内的所有产品。
  *
  * @author Michael.Chu
  * @date 2019-04-09 22:45
@@ -12,8 +13,12 @@ public class AppTest {
 
     @Test
     public void abstractFactoryTest() {
-        CourseFactory factory = new JavaCourseFactory();
-        factory.getArticle();
-        factory.getVideo();
+        CourseFactory javaFactory = new JavaCourseFactory();
+        javaFactory.getArticle().produce();
+        javaFactory.getVideo().produce();
+
+        CourseFactory pythonFactory = new PythonCourseFactory();
+        pythonFactory.getArticle().produce();
+        pythonFactory.getVideo().produce();
     }
 }
