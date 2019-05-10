75e8a6957cf1afb9a29de6de2d210f61178def9c
chudichen <chudichen.foxmail.com>
2019-05-10 15:30:52 +0800

    添加建造者模式

---
+++ src/main/java/com/study/design/pattern/creational/builder/App.java
@@ -0,0 +1,15 @@
+package com.study.design.pattern.creational.builder;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Michael Chu
+ * @date 2019-05-10 14:15
+ */
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/study/design/pattern/creational/builder/HairType.java
@@ -0,0 +1,8 @@
+package com.study.design.pattern.creational.builder;
+
+/**
+ * @author Michael Chu
+ * @date 2019-05-10 14:56
+ */
+public class HairType {
+}
